5fe712f3b87bbbdeee1fb3f1aabe9073ba5a8781
melerodaw <melerodaw@gmail.com>
2026-01-19 14:28:14 +0100

    lunes 19

--- ejercicio1/src/ejercicio1/Empresa.java
+++ ejercicio1/src/ejercicio1/Empresa.java
@@ -1,14 +1,59 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ejercicio1;
-
-/**
- *
- * @author EAG
- */
 public class Empresa {
+    // Declaración de atributos
+    private Vehiculo[] vehiculosRegistrados;
+
+    // Constructor por defecto
+    public Empresa() {
+        this.vehiculosRegistrados = new Vehiculo[0];
+    }
+
+    // Constructor por parámetros
+    public Empresa(Vehiculo[] vehiculosRegistrados) {
+        this.vehiculosRegistrados = vehiculosRegistrados;
+    }
+    
+    // Constructor de copia
+    public Empresa(Empresa e) {
+        this.vehiculosRegistrados = e.vehiculosRegistrados;
+    }
+    
+    // Declaración de getters (1 por atributo)
+    public Vehiculo[] getVehiculosRegistrados() {
+        return vehiculosRegistrados;
+    }
+
+    // Declaración de setters (1 por atributo)
+    public void setVehiculosRegistrados(Vehiculo[] vehiculosRegistrados) {
+        this.vehiculosRegistrados = vehiculosRegistrados;
+    }
+    
+    // Declaración de métodos personalizados
+    public boolean comprobarVehiculo(Vehiculo[] vR, Vehiculo v){
+        for(int i=0; i < vR.length; i++){
+            if(vR[i].getMatricula().equals(v.getMatricula())){
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    public void incorporarVehiculo(Vehiculo[] vR, Vehiculo v){
+        if(comprobarVehiculo(vR, v) == false){
+            
+        }
+    } 
+            
+          
+//+ alquilarVehiculo(v: vehiculo): void
+
+
+    // Declaración toString() (con @Override)
+    @Override
+    public String toString() {
+        return "Empresa{" + "vehiculosRegistrados=" + vehiculosRegistrados + '}';
+    }
     
 }
+
+
--- ejercicio1/src/ejercicio1/Vehiculo.java
+++ ejercicio1/src/ejercicio1/Vehiculo.java
@@ -1,14 +1,90 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ejercicio1;
-
-/**
- *
- * @author EAG
- */
 public class Vehiculo {
+    
+// Declaración de atributos
+    private String matricula;
+    private String marca;
+    private String modelo;
+    private boolean cocheDisponible;
+    
+// Constructor por defecto
+
+    public Vehiculo() {
+        this.matricula = "";
+        this.marca = "";
+        this.modelo = "";
+        this.cocheDisponible = true;
+    }
+    
+// Constructor por parámetros
+    public Vehiculo(String matricula, String marca, String modelo, boolean cocheDisponible) {
+        this.matricula = matricula;
+        this.marca = marca;
+        this.modelo = modelo;
+        this.cocheDisponible = cocheDisponible;
+    }
+    
+// Constructor de copia
+    public Vehiculo(Vehiculo v){
+        this.matricula = v.matricula;
+        this.marca = v.marca;
+        this.modelo = v.modelo;
+        this.cocheDisponible = v.cocheDisponible;
+        
+        //if(v.)
+    }
+    
+// Declaración de getters (1 por atributo)
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public boolean isCocheDisponible() {
+        return cocheDisponible;
+    }
+    
+// Declaración de setters (1 por atributo)
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public void setCocheDisponible(boolean cocheDisponible) {
+        this.cocheDisponible = cocheDisponible;
+    }
+    
+// Declaración de métodos personalizados
+    
+    
+// Declaración toString() (con @Override)
+    @Override
+    public String toString() {
+        return "Vehiculo{" + 
+                "matricula=" + matricula + 
+                ", marca=" + marca + 
+                ", modelo=" + modelo + 
+                ", cocheDisponible=" + cocheDisponible + '}';
+    }
+
+    
+
+
+    
+
     
 }
